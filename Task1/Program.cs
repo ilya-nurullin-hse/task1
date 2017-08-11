04f83c9077e4f884c29c1bf2fa585fd9b1c77ba0
Ilya Nurullin <nurullinin@mail.ru>
2017-08-11 19:46:33 +0500

    Add tricky optimization. Accepted answer.

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Task1
 {
@@ -11,22 +8,82 @@ namespace Task1
     {
         static void Main(string[] args)
         {
-            int[] inputs = File.ReadLines("INPUT.TXT").First().Split(' ').Select(int.Parse).ToArray();
-            int N = inputs[0];
-            int A = inputs[1];
-            int B = inputs[2];
-
-            Console.WriteLine(C(N+1, A)* C(N + 1, B));
+            byte[] inputs = Console.ReadLine().Split(' ').Select(x => byte.Parse(x)).ToArray();
+            byte N = inputs[0];
+            byte A = inputs[1];
+            byte B = inputs[2];
+            
+            Console.WriteLine(C((byte) (N + 1), A) * C((byte) (N + 1), B));
         }
 
-        static int C(int n, int m)
+        static decimal C(byte n, byte m)
         {
-            return Factorial(n + m - 1) / (Factorial(n - 1) * Factorial(m));
+            var a1 = getFactorialMultipliersWithStop((byte) (n + m - 1), (byte) Math.Max(n - 1, m));
+            var a2 = getFactorialMultipliers((byte) Math.Min(n - 1, m));
+            var t = Simplify(a1, a2);
+            var r1 = Multiply(t.Item1);
+            var r2 = Multiply(t.Item2);
+            return r1 / r2;
         }
 
-        static int Factorial(int x)
+        static byte[] getFactorialMultipliers(byte n)
         {
-            return (x == 0) ? 1 : x * Factorial(x - 1);
+            return getFactorialMultipliersWithStop(n, 1);
+        }
+
+        static byte[] getFactorialMultipliersWithStop(byte n, byte stop)
+        {
+            if (n < 2) return new byte[] {1};
+
+            byte[] temp = new byte[n-stop];
+            for (byte i = n; i > stop; i--)
+            {
+                temp[n-i] = i;
+            }
+            return temp;
+        }
+
+        static decimal Multiply(byte[] ms)
+        {
+            decimal res = 1;
+            foreach (var m in ms)
+            {
+                res *= m;
+            }
+            return res;
+        }
+
+        static Tuple<byte[], byte[]> Simplify(byte[] greater, byte[] less)
+        {
+            List<byte> _g = greater.ToList();
+            List<byte> _l = less.ToList();
+
+            for (int i = 0; i < _g.Count; i++)
+            {
+                for (int j = 0; j < _l.Count; j++)
+                {
+                    if (_g[i] % _l[j] == 0)
+                    {
+                        _g[i] /= _l[j];
+                        _l.RemoveAt(j);
+                        j--;
+                    }
+                }
+            }
+
+            return new Tuple<byte[], byte[]>(_g.ToArray(), _l.ToArray());
+        }
+    }
+
+    class Tuple<A1, A2>
+    {
+        public A1 Item1;
+        public A2 Item2;
+
+        public Tuple(A1 item1, A2 item2)
+        {
+            Item1 = item1;
+            Item2 = item2;
         }
     }
 }
